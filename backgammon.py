2ba22edba52e181baffab916815cbce7c2266072
Brad Corwin <Brad Corwin>
2017-03-13 12:23:13 -0500

    Added a second determine_winner function, finished opponent_doubled and opponent_resigned scenarios

--- backgammon.py
+++ backgammon.py
@@ -19,6 +19,15 @@ def calculate_pip_count():
     return player_pip, opponent_pip;
 
 def determine_winner():
+    board = gnubg.board()
+    opponent_board, player_board = board
+    temp1 = all(player_board == 0 for item in board)
+    temp2 = all(opponent_board == 0 for item in board)
+    print(temp1)
+    print(temp2)
+    return temp1, temp2;
+
+def determine_winner_1():
     match = gnubg.match()
     match_info = match["games"]
     return match_info[0]['info']['winner'];
@@ -45,67 +54,82 @@ def probability():
 print("Starting Game")
 gnubg.command('new game')
 #gnubg.command('show pipcount')
-reward = 0 # reward for current game
+total_reward = 0
+reward = 0
 
 # Testing
-#cubeinfo = gnubg.positionkey()
-#cube_owner = cubeinfo["analysis"]
-#pprint(cubeinfo)
-
-# Monitor player board layout
-#past_player_board = [0] * 25
-#board = gnubg.board()
-#opponent_board, player_board = board
-#temp1 = all(player_board == 0 for item in board)
-#temp2 = all(player_board == 0 for item in board)
-#if (player_board != past_player_board):
-#    print("not equal")
+cubeinfo = gnubg.cubeinfo()
+#cube_owner = cubeinfo['doubled']
+pprint(cubeinfo)
+#print(type(cubeinfo))
 
 while True:
-    # Breaks the loop when a winner has been determined
-    if (str(determine_winner()) != "None"):
-        print("Game Completed!")
-        break;
+    posinfo = gnubg.posinfo()
+    cubeinfo = gnubg.cubeinfo()
+
+    #cubeinfo = gnubg.match()
+    #cube_owner = cubeinfo["games"]
+    #cubes = cube_owner[0]['game'] #action, dice, player
+    #cubey = cubes[0]['action']
+    #cubes = cube_owner[0]['info']['score-O'] #score-O, score-X, winner
+    #pprint(cubey)
+
+    #cubeinfo = gnubg.match()
+    #cube_owner = cubeinfo["match-info"]
+    #pprint(cube_owner)
 
     # Decides whether to accept the oppoent's double
-    '''
-    if opponent doubles:
+    opponent_doubled = posinfo['doubled']
+    if opponent_doubled:
         double, reward = decide_on_cube()
         if double:
+            print("Player accepts double.")
             gnubg.command('accept')
         else:
+            print("Player rejects double, and loses match.")
+            player_wins_game = False
             gnubg.command('reject')
-    '''
+            break;
+        total_reward += reward
 
     # Accepts the resignation when the opponent offers to resign
-    '''
-    if opponent resigns:
+    opponent_resigns = posinfo['resigned']
+    if opponent_resigns:
+        print("Opponent resigns!")
+        player_wins_game = True
         gnubg.command('accept')
-    '''
+        break;
 
     # Cubeowner = 1 if opponent, -1 if no one, 0 if player
-    #cube_in_control = cube owner -1 or 0
+    opponent_has_cube = cubeinfo['cubeowner'] == 1
 
     # Decides whether player should double before rolling
-    '''
     double, reward = decide_on_cube()
-    #if double and cube_in_control:
+    if double and not opponent_has_cube:
+        total_reward += reward
         #gnubg.command('double')
-    '''
 
     gnubg.command('roll')
 
-    # Commands the best move if a move is possible
-    #if can_move
+    # Commands an optimal move if a move is possible
+    #players_turn = posinfo['turn']
+    #if players_turn:
     gnubg.command('move ' + gnubg.movetupletostring(gnubg.findbestmove(gnubg.board(), gnubg.cubeinfo()), gnubg.board()))
 
-# temporary value, will be determined if player wins
-win_game = True
+    # Breaks the loop when a winner has been determined
+    #if (str(determine_winner_1()) != 'None'):
+    temp1, temp2 = determine_winner()
+    if temp1 or temp2:
+        print("Game Completed!")
+        print(str(determine_winner_1()))
+        break;
+
 doubling_cube_value = find_doubling_cube_value()
 
-if win_game:
-    reward = 1 * (doubling_cube_value + 1000)
+if True:#player_wins_game:
+    reward = 1000 * doubling_cube_value
 else:
-    reward = -1 * (doubling_cube_value + 1000)
+    reward = -1000 * doubling_cube_value
 
-#print(reward)
+total_reward += reward
+print(total_reward)
