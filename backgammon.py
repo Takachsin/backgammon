8bf90678513537de3a0853981f89669ad044c4a9
Brad Corwin <Brad Corwin>
2017-03-13 14:49:07 -0500

    Added the probability function

--- backgammon.py
+++ backgammon.py
@@ -7,7 +7,8 @@ def find_doubling_cube_value():
     return doubling_cube_value;
 
 # Calculate the number of spaces remaining to bear off all checkers
-def calculate_pip_count():
+def calc_pip_count():
+    doubling_cube_value = find_doubling_cube_value()
     player_pip = 0
     opponent_pip = 0
     board = gnubg.board()
@@ -16,24 +17,28 @@ def calculate_pip_count():
         player_pip = player_pip + player_board[x] * (x+1)
     for x in xrange (0,24):
         opponent_pip = opponent_pip + opponent_board[x] * (x+1)
-    return player_pip, opponent_pip;
+    if player_pip > opponent_pip:
+        reward = 1 * doubling_cube_value
+    elif player_pip < opponent_pip:
+        reward = -1 * doubling_cube_value
+    else:
+        reward = 0
+    return player_pip, opponent_pip, reward;
 
 def determine_winner():
     board = gnubg.board()
     opponent_board, player_board = board
     temp1 = all(player_board == 0 for item in board)
     temp2 = all(opponent_board == 0 for item in board)
-    print(temp1)
-    print(temp2)
     return temp1, temp2;
 
 def determine_winner_1():
     match = gnubg.match()
-    match_info = match["games"]
+    match_info = match['games']
     return match_info[0]['info']['winner'];
 
-def decide_on_cube():
-    player_pip, opponent_pip = calculate_pip_count()
+def decide_on_double():
+    player_pip, opponent_pip, reward = calc_pip_count()
     doubling_cube_value = find_doubling_cube_value()
     if player_pip > opponent_pip:
         double = True
@@ -44,11 +49,28 @@ def decide_on_cube():
     else:
         double = False
         reward = 0
-    return double, reward;
+    return double;
 
-# Defines the probability off winning
-def probability():
-    return;
+# Defines the probability of rolling a specific number
+def prob_of_rolling(num):
+    total_combinations = 36
+    prob = 0.00
+    combinations = 0
+    for die1 in range(1,7):
+        if die1 == num:
+            combinations += 1
+    for die2 in range(1,7):
+        if die2 == num:
+            combinations += 1
+    for die1 in range(1,7):
+        for die2 in range(1,7):
+            if (die1 + die2) == num:
+                combinations += 1
+    prob = combinations / float(total_combinations)
+    return prob;
+
+#Q(st, at) = Q(st, at) + alpha*(rt+gamma*max(Q(st+1, a) - Q(st, at)))
+#SARSA - Q(st, at) = Q(st, at) + alpha*(rt+gamma*max(Q(st+1, at+1) - Q(st, at)))
 
 # Start the game
 print("Starting Game")
@@ -56,16 +78,27 @@ gnubg.command('new game')
 #gnubg.command('show pipcount')
 total_reward = 0
 reward = 0
+learning_rate = 0.5
+doubling_cube_values = [1, 2, 4, 8, 16, 32, 64]
+
+prob_of_rolling(5)
+#print(prob_of_rolling(2))
 
 # Testing
-cubeinfo = gnubg.cubeinfo()
+#cubeinfo = gnubg.cubeinfo()
 #cube_owner = cubeinfo['doubled']
-pprint(cubeinfo)
+#pprint(cubeinfo)
 #print(type(cubeinfo))
 
-while True:
+#s: turn until game ends
+#a: double, don't double, accept double, reject double
+
+while False:
     posinfo = gnubg.posinfo()
     cubeinfo = gnubg.cubeinfo()
+
+    player_pip, opponent_pip, reward = calc_pip_count()
+    total_reward += reward
 
     #cubeinfo = gnubg.match()
     #cube_owner = cubeinfo["games"]
@@ -81,7 +114,7 @@ while True:
     # Decides whether to accept the oppoent's double
     opponent_doubled = posinfo['doubled']
     if opponent_doubled:
-        double, reward = decide_on_cube()
+        double = decide_on_double()
         if double:
             print("Player accepts double.")
             gnubg.command('accept')
@@ -90,7 +123,6 @@ while True:
             player_wins_game = False
             gnubg.command('reject')
             break;
-        total_reward += reward
 
     # Accepts the resignation when the opponent offers to resign
     opponent_resigns = posinfo['resigned']
@@ -104,7 +136,7 @@ while True:
     opponent_has_cube = cubeinfo['cubeowner'] == 1
 
     # Decides whether player should double before rolling
-    double, reward = decide_on_cube()
+    double = decide_on_double()
     if double and not opponent_has_cube:
         total_reward += reward
         #gnubg.command('double')
@@ -127,9 +159,9 @@ while True:
 doubling_cube_value = find_doubling_cube_value()
 
 if True:#player_wins_game:
-    reward = 1000 * doubling_cube_value
+    reward = 1 * (doubling_cube_value + 1000)
 else:
-    reward = -1000 * doubling_cube_value
+    reward = -1 * (doubling_cube_value + 1000)
 
 total_reward += reward
 print(total_reward)
